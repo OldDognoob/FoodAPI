21499824eba4258ca574c9aa3e3e8883eaf46c98
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-04-29 19:08:36 +0300

    adding-api

--- my-recipe/src/App.js
+++ my-recipe/src/App.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 import "./App.css";
 
 const App = () => {
-    return (
-        <div>
-            <h1>Searching Food Recipe</h1>
-        </div>
-    )
-}
+  const APP_ID = "1c999786";
 
-export default App
+  const APP_KEY ="6f0457310adcf00c7cfbe5ad8746b21e";
+
+  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  return (
+    <div className="App">
+      <h1>Searching Food Recipe</h1>
+    </div>
+  );
+};
+
+export default App;
