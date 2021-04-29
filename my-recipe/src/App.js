04cc467f99c816453ff1190d01b0aee8346be2c9
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-04-29 19:11:21 +0300

    wip-foo-api

--- my-recipe/src/App.js
+++ my-recipe/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Axios from 'axios';
 import "./App.css";
 
 const App = () => {
@@ -7,9 +8,15 @@ const App = () => {
   const APP_KEY ="6f0457310adcf00c7cfbe5ad8746b21e";
 
   const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  
+  const getData = async () => {
+   const result = await Axios.get(url);
+
+   console.log(result)
+  }
   return (
     <div className="App">
-      <h1>Searching Food Recipe</h1>
+      <h1 onClick={getData}>Searching Food Recipe</h1>
     </div>
   );
 };
